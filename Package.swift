3456cd98436615284964bcc86cfd38e4b2794c32
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2024-04-20 00:45:07 -0300

    Exclude last?

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "TestPackage",
-            path: "Sources"
+            path: "Sources",
+            exclude: ["../TestAppExamplePackage", "../Tests"]
         ),
         .testTarget(
             name: "TestPackageTests",
