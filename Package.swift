cecbd1bc6f95e0999b8f07514b75afae9069ba29
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2024-04-20 00:17:25 -0300

    Exclude More

--- Package.swift
+++ Package.swift
@@ -16,11 +16,13 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TestPackage"
+            name: "TestPackage",
+            path: "Sources"
         ),
         .testTarget(
             name: "TestPackageTests",
-            dependencies: ["TestPackage"]
+            dependencies: ["TestPackage"], 
+            path: "Tests"
         ),
     ]
 )
